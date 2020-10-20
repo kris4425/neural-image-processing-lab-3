be3b8d95d590f9c442eafca7131c8c108188f6c7
kris4425 <46685995+kris4425@users.noreply.github.com>
2020-10-20 12:43:08 -0400

    Add files via upload

---
+++ Lab02.py
@@ -0,0 +1,192 @@
+import os
+import numpy as np
+import tensorflow as tf
+from tensorflow import keras
+import tensorflow.keras.backend as K
+import random
+from imageio import imsave
+from skimage.transform import resize
+from scipy.optimize import fmin_l_bfgs_b   # https://docs.scipy.org/doc/scipy/reference/generated/scipy.optimize.fmin_l_bfgs_b.html
+from tensorflow.keras.applications import vgg19
+from tensorflow.keras.preprocessing.image import load_img, img_to_array
+import warnings
+
+# suppress tf eager execution warming
+tf.compat.v1.disable_eager_execution()
+
+random.seed(1618)
+np.random.seed(1618)
+#tf.set_random_seed(1618)   # Uncomment for TF1.
+tf.random.set_seed(1618)
+
+#tf.logging.set_verbosity(tf.logging.ERROR)   # Uncomment for TF1.
+os.environ['TF_CPP_MIN_LOG_LEVEL'] = '2'
+
+CONTENT_IMG_PATH = "bird.jpg"           #TODO: Add this.
+STYLE_IMG_PATH = "giger.jpg"             #TODO: Add this.
+
+CONTENT_IMG_H = 500
+CONTENT_IMG_W = 500
+
+STYLE_IMG_H = 500
+STYLE_IMG_W = 500
+
+CONTENT_WEIGHT = 0.1    # Alpha weight.
+STYLE_WEIGHT = 1.0      # Beta weight.
+TOTAL_WEIGHT = 1.0
+
+TRANSFER_ROUNDS = 5
+
+#=============================<Evaluation Fuctions>=================================
+class Evaluator(object):
+    def __init__(self, func):
+        self.loss_value = None
+        self.grad_values = None
+        self.func = func
+
+    def loss(self, x):
+        x = x.reshape((1, CONTENT_IMG_H, CONTENT_IMG_W, 3))
+        self.loss_value, self.grad_values = self.func([x])
+        return self.loss_value
+
+    def grad(self, x):
+        return self.grad_values.flatten().astype(np.float64)
+
+#=============================<Helper Fuctions>=================================
+'''
+TODO: implement this.
+This function should take the tensor and re-convert it to an image.
+'''
+def deprocessImage(img):
+    img = img.reshape((CONTENT_IMG_H), (CONTENT_IMG_W), 3)
+
+    # add in average
+    img[:, :, 0] += 103.939
+    img[:, :, 1] += 116.779
+    img[:, :, 2] += 123.68
+
+    img = img[:, :, ::-1]
+    img = np.clip(img, 0, 255).astype('uint8')
+    return img
+
+
+
+def gramMatrix(x):
+    features = K.batch_flatten(K.permute_dimensions(x, (2, 0, 1)))
+    gram = K.dot(features, K.transpose(features))
+    return gram
+
+#========================<Loss Function Builder Functions>======================
+
+def styleLoss(style, gen):
+    numFilters = 3
+    return K.sum(K.square(gramMatrix(style) - gramMatrix(gen))) / (4. * (numFilters ** 2) * ((CONTENT_IMG_H * CONTENT_IMG_W) ** 2))   #TODO: implement.
+
+
+def contentLoss(content, gen):
+    return K.sum(K.square(gen - content))
+
+
+def totalLoss(x):
+    return K.sum(K.square(x[:, :CONTENT_IMG_H - 1, :CONTENT_IMG_W - 1, :] - x[:, 1:, :CONTENT_IMG_W - 1, :])
+                 + K.square(x[:, :CONTENT_IMG_H - 1, :CONTENT_IMG_W - 1, :] - x[:, :CONTENT_IMG_H - 1, 1:, :]))   #TODO: implement.
+
+
+#=========================<Pipeline Functions>==================================
+
+def getRawData():
+    print("   Loading images.")
+    print("      Content image URL:  \"%s\"." % CONTENT_IMG_PATH)
+    print("      Style image URL:    \"%s\"." % STYLE_IMG_PATH)
+    cImg = load_img(CONTENT_IMG_PATH)
+    tImg = cImg.copy()
+    sImg = load_img(STYLE_IMG_PATH)
+    print("      Images have been loaded.")
+    return ((cImg, CONTENT_IMG_H, CONTENT_IMG_W), (sImg, STYLE_IMG_H, STYLE_IMG_W), (tImg, CONTENT_IMG_H, CONTENT_IMG_W))
+
+
+
+def preprocessData(raw):
+    img, ih, iw = raw
+    img = img_to_array(img)
+    with warnings.catch_warnings():
+        warnings.simplefilter("ignore")
+        img = resize(img, (ih, iw, 3))
+    img = img.astype("float64")
+    img = np.expand_dims(img, axis=0)
+    img = vgg19.preprocess_input(img)
+    return img
+
+
+'''
+TODO: Allot of stuff needs to be implemented in this function.
+First, make sure the model is set up properly.
+Then construct the loss function (from content and style loss).
+Gradient functions will also need to be created, or you can use K.Gradients().
+Finally, do the style transfer with gradient descent.
+Save the newly generated and deprocessed images.
+'''
+def styleTransfer(cData, sData, tData):
+    print("   Building transfer model.")
+    contentTensor = K.variable(cData)
+    styleTensor = K.variable(sData)
+    genTensor = K.placeholder((1, CONTENT_IMG_H, CONTENT_IMG_W, 3))
+    inputTensor = K.concatenate([contentTensor, styleTensor, genTensor], axis=0)
+    model = vgg19.VGG19(include_top=False, weights="imagenet", input_tensor=inputTensor)  #TODO: implement.
+    outputDict = dict([(layer.name, layer.output) for layer in model.layers])
+
+    print("   VGG19 model loaded.")
+    loss = 0.0
+    styleLayerNames = ["block1_conv1", "block2_conv1", "block3_conv1", "block4_conv1", "block5_conv1"]
+    contentLayerName = "block5_conv2"
+
+    print("   Calculating content loss.")
+    contentLayer = outputDict[contentLayerName]
+    contentOutput = contentLayer[0, :, :, :]
+    genOutput = contentLayer[2, :, :, :]
+    loss += CONTENT_WEIGHT * contentLoss(contentOutput, genOutput)    #TODO: implement.
+
+    print("   Calculating style loss.")
+    for layerName in styleLayerNames:
+        styleLayer = outputDict[layerName]
+        styleOutput = styleLayer[1, :, :, :]
+        genOutput = styleLayer[2, :, :, :]
+        loss += (STYLE_WEIGHT/len(styleLayerNames)) * styleLoss(styleOutput, genOutput)   #TODO: implement.
+    loss += TOTAL_WEIGHT * totalLoss(genTensor)   #TODO: implement.
+
+    # TODO: Setup gradients or use K.gradients().
+    gradient = K.gradients(loss, genTensor)
+    x = cData
+    graphExecutionFunction = K.function([genTensor], [loss] + gradient)
+    evaluator = Evaluator(func=graphExecutionFunction)
+
+    print("   Beginning transfer.")
+    for i in range(TRANSFER_ROUNDS):
+        print("   Step %d." % i)
+        #TODO: perform gradient descent using fmin_l_bfgs_b.
+        x, tLoss, d = fmin_l_bfgs_b(evaluator.loss, x.flatten(), evaluator.grad, maxiter=5)
+        print("      Loss: %f." % tLoss)
+        img = deprocessImage(x.copy())
+        saveFile = 'iteration' + str(i) + '.jpg'   #TODO: Implement.
+        imsave(saveFile, img)   #Uncomment when everything is working right.
+        print("      Image saved to \"%s\"." % saveFile)
+    print("   Transfer complete.")
+
+
+#=========================<Main>================================================
+
+def main():
+    print("Starting style transfer program.")
+    raw = getRawData()
+
+    print(raw)
+    cData = preprocessData(raw[0])   # Content image.
+    sData = preprocessData(raw[1])   # Style image.
+    tData = preprocessData(raw[2])   # Transfer image.
+    styleTransfer(cData, sData, tData)
+    print("Done. Goodbye.")
+
+
+
+if __name__ == "__main__":
+    main()
